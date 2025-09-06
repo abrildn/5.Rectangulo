8186715c07eb0c1d5e7483003434e10d6d1a401a
abrildn <abrudomingueznoya@gmail.com>
2025-09-06 20:01:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Rectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la altura del rectángulo");
+            string linea = Console.ReadLine();
+            int altura = int.Parse(linea);
+
+            Console.WriteLine("Ingrese la base del rectángulo");
+            linea = Console.ReadLine();
+            int base1 = int.Parse(linea);
+
+            int area = base1 * altura;
+            Console.WriteLine("El área del rectángulo es:");
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+}
